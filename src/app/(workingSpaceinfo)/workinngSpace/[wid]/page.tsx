bd8ced2d298b8b8bc8b68dc4025d8b341ea32218
PeemSx <peemp5802@gmail.com>
2024-03-24 22:21:32 +0700

    update picture fetching system

--- src/app/(workingSpaceinfo)/workinngSpace/[wid]/page.tsx
+++ src/app/(workingSpaceinfo)/workinngSpace/[wid]/page.tsx
@@ -1,24 +1,27 @@
+
 import getWorkingSpace from "@/libs/getWorkingSpace";
 import Image from "next/image";
 import React from "react";
 
-async function page({ params }: { params: { wid: string } }) {
+
+async function page({ params }: { params: { wid: string} }) {
   const workingSpaceData = (await getWorkingSpace(params.wid))
     .data as WorkingSpaceItem;
 
+  // console.log(workingSpaceData)
+
   return (
-    <div className="flex  my-4 w-[60%] mx-auto items-center px-[16px] py-[24px] rounded-xl shadow-xl gap-5">
-      {/* <img
-        src={hospitalData.picture}
-        alt="hospital_img"
+    <div className="bg-slate-700 flex m-6 h-[100vh] items-center px-6 py-[24px] rounded-xl shadow-xl gap-5">
+
+      {/* <Image
+        src={workingSpaceData.picture}
+        alt="workingSpace cover pic"
+        width={0}
+        height={200}
         className="mx-auto my-4 w-[600px]"
       /> */}
-      <Image
-        src={workingSpaceData.picture}
-        alt=""
-        width={0}
-        height={0}
-        className="mx-auto my-4 w-[600px]"
+      <img src={workingSpaceData.picture}
+      className="h-full w-auto rounded-lg"
       />
       <div className="relative flex flex-col ">
         <h1 className="mx-auto my-2 text-2xl w-full">
--- src/components/WorkingSpaceCatalog.tsx
+++ src/components/WorkingSpaceCatalog.tsx
@@ -14,14 +14,14 @@ async function WorkingSpaceCatalog({
   return (
     <div className="relative flex flex-col my-4 justify-evenly bg-slate-700 rounded-xl m-6 p-4">
       {workingSpaceItems.data.map((workingSpaceItem) => (
-        <div className="">
+        <div className="" key={workingSpaceItem._id}>
           <Link
             href={"/workinngSpace/" + workingSpaceItem._id}
             key={workingSpaceItem._id}
           >
             <Card
               hospitalName={workingSpaceItem.name}
-              address= {workingSpaceItem.address +" "+ workingSpaceItem.district +" " +  workingSpaceItem.province +" " + workingSpaceItem.postalcode}
+              address= {workingSpaceItem.address + " " + workingSpaceItem.district + " " +  workingSpaceItem.province + " " + workingSpaceItem.postalcode}
               imgSrc={`/images/workingPic${i++}.jpg`}
             />
           </Link>
